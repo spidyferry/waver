dc929bc59339e479e1f37e68b21ae1b7b848081f
Nirwan Ramdani <nirwan.nrs@gmail.com>
2021-04-24 12:36:48 +0700

    Initial Commit

---
+++ BLBWaterTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLBWaterTracker
+//
+//  Created by Nirwan Ramdani on 24/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
